aef1853e70fce65ea76f1662c9a5b8e1dd7cb990
minjunkim0205 <minjunkim0205@gmail.com>
2023-09-11 13:03:19 +0900

    Update JavaPrograming2-MiyoungShin

---
+++ src/Week2Practice/CodingTest.java
@@ -0,0 +1,6 @@
+package Week2Practice;
+
+public class CodingTest {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/Week2Practice/Info.java
@@ -0,0 +1,12 @@
+package Week2Practice;
+
+public class Info {
+    // Private
+    // Public
+    public String id;
+    public Integer pass;
+
+    public Info(String id) {
+        this.id = id;
+    }
+}
