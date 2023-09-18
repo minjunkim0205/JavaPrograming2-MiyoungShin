7523c8a9e80eb797bd853b171a7a423318287369
minjunkim0205 <minjunkim0205@gmail.com>
2023-09-18 15:20:42 +0900

    Update Week4Practice

--- src/Week4Practice/CodingTest.java
+++ src/Week4Practice/CodingTest.java
@@ -18,10 +18,24 @@ public class CodingTest {
         // Solve,Output
         System.out.print("===== 참가번호 정렬 전 =====\n");
         for (int i = 0; i < SIZE; i++){
-            System.out.print("name = "+); // TODO: 2023-09-18  
+            System.out.print("name = "+name[i]+"\t"+"num = "+number[i]+"\n");
         }
-
+        for (int p = 1; p < SIZE; p++){
+            for (int j = 0; j < SIZE-p; j++){
+                if (number[j] > number[j+1]){
+                    int temp_i = number[j];
+                    number[j] = number[j+1];
+                    number[j+1] = temp_i;
+                    String temp_s = name[j];
+                    name[j] = name[j+1];
+                    name[j+1] = temp_s;
+                }
+            }
+        }
         System.out.print("===== 참가번호 정렬 후 =====\n");
+        for (int i = 0; i < SIZE; i++){
+            System.out.print("name = "+name[i]+"\t"+"num = "+number[i]+"\n");
+        }
         input.close();
     }
 }
