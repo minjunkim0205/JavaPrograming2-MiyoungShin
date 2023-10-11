e9b31b496a5bb8993fcf0d8246f171921f3e6f01
minjunkim0205 <minjunkim0205@gmail.com>
2023-10-11 15:40:01 +0900

    Update Week06Theory

---
+++ src/MidtermExam/Te01.java
@@ -0,0 +1,4 @@
+package MidtermExam;
+
+public class Te01 {
+}
---
+++ src/Week06Theory/Ex03/Test.java
@@ -0,0 +1,13 @@
+package Week06Theory.Ex03;
+
+public class Test {
+    public static void main(String[] args) {
+        Ano ano = new Ano();
+        ano.method1(new Person() {
+            public void wake() {
+                System.out.print("6시에 일어납니다\n");
+            }
+        });
+        ano.field.wake();
+    }
+}
